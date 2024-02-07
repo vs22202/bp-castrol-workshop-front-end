b87853750a43457c71c2c9caf0cfc968e499abe1
W4ddNJ5n76JHfk54QOyaCg9L0jqj6VZTziVleuK6yrw= <Falcon_Code_user+1-at-282572457531>
2024-02-07 21:29:55 +0530

    added button styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,44 @@ function App() {
   return (
     <>
       <h1>Component</h1>
-      <Button size="sm"/>
+
+      <div className="button-container">
+        <div className="button-row">
+          <Button state="active" size="sm"/>
+          <Button state="active" size="md"/>
+          <Button state="active" size="lg"/>
+        </div>
+
+        <div className="button-row">
+          <Button state="hover" size="sm"/>
+          <Button state="hover" size="md"/>
+          <Button state="hover" size="lg"/>
+        </div>
+
+        <div className="button-row">
+          <Button state="disabled" size="sm"/>
+          <Button state="disabled" size="md"/>
+          <Button state="disabled" size="lg"/>
+        </div>
+
+        <div className="button-row">
+          <Button solid={false} state="active" size="sm"/>
+          <Button solid={false}  state="active" size="md"/>
+          <Button solid={false} state="active" size="lg"/>
+        </div>
+
+        <div className="button-row">
+          <Button solid={false} state="hover" size="sm"/>
+          <Button solid={false} state="hover" size="md"/>
+          <Button solid={false} state="hover" size="lg"/>
+        </div>
+
+        <div className="button-row">
+          <Button solid={false} state="disabled" size="sm"/>
+          <Button solid={false} state="disabled" size="md"/>
+          <Button solid={false} state="disabled" size="lg"/>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/ButtonComponent/Button.tsx
+++ src/components/ButtonComponent/Button.tsx
@@ -4,9 +4,11 @@ import styles from "./Button.module.css";
 /** The props type of {@link Button | `Button`}. */
 export type ButtonProps = {
   /**
-   * Size of the buttons
+   * fill, state and size of the buttons
    */
-  size: string;
+  solid?: boolean;
+  state: "active" | "hover" | "disabled";
+  size: "sm" | "md" | "lg";
 };
 
 /**
@@ -21,14 +23,47 @@ export type ButtonProps = {
  * <Button size="sm"/>
  * ```
  */
-export function Button ({ size }: ButtonProps ){
+
+function changeColorWithState(varColor:string, state:string, isShadow:boolean){
+  switch(state){
+    case "active":
+        varColor = !isShadow ? "var(--primary---main500)" : "var(--primaryshdw--md)";
+        break;
+      case "hover":
+        varColor = !isShadow ? "var(--primary---main300)" : "var(--primaryshdw--lg)";
+        break;
+      case "disabled":
+        varColor = !isShadow ? "var(--neutral---main150)" : "var(--neutralshdw--lg)";
+        break;
+      default:
+        varColor = "white";
+  }
+  return varColor
+}
+export function Button ({ solid=true, state="active", size }: ButtonProps ){
+
+  const bgcolor = changeColorWithState("", state, false)
+  const fontcolor = solid ? "var(--shades---main-white)" : changeColorWithState("", state, false)
+  const shadowcolor = changeColorWithState("", state, true)
+
+  const buttonStyle = {
+    padding: "var(--spacing-2xs)",
+    borderRadius: " var(--spacing-4xs)",
+    backgroundColor: solid ? bgcolor : "var( --shades---main-white)",
+    border: solid ? "none" : `2px solid ${bgcolor}`,
+    color : fontcolor,
+    boxShadow: `2px ${shadowcolor}`
+  }
+  
   return (
     <>
-      {size == "sm" ? (
-        <button className={`${styles.sm} ${styles.outline}`}>Button</button>
-      ) : (
-        <button className={`${styles.sm} ${styles.outline}`}>Button</button>
-      )}
+      <button 
+        className={`${styles.solid} ${styles[size]}`}
+        style={buttonStyle}
+        disabled={state==='disabled'}
+      >
+        Button
+      </button>
     </>
   );
 };
