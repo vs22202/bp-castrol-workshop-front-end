d26afe68543128241671e1c1e246ad8332df0dd3
Rishika Agrawal <89201634+Rishika631@users.noreply.github.com>
2024-02-20 16:40:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import {Navbar} from "./components/Navbar/navbar";
 import { Button } from './components/ButtonComponent/Button'
 import {InputField} from './components/InputFieldComponent/InputField'
-import { ListItem } from "./components/Q&AComponent/ListeItem";
+import { ListItem } from "./components/Q&AComponent/ListItem";
+import { List } from "./components/Q&AComponent/List";
 import {FooterWithLogo} from "./components/FooterComponent/Footer"
 import { Checkbox } from "./components/CheckboxComponent/CheckboxComponent";
 import './App.css'
@@ -9,7 +10,8 @@ import './App.css'
 
 function App() {
 
-  const checkboxtext = "I consent to having my data processed according to the privacy statement"
+  /*for list
+  const items = ['Are you committed to quality maintenance and friendly customer service?', 'Do you have at least 3 bays in your workshop?', 'Are you a full service workshop?', 'Are you ready to benefit from branding with Castrol?'];*/
 
   return (
     <>
@@ -48,13 +50,16 @@ function App() {
       <InputField type="text" label='Label' size='sm' isWrong={true}/>
       <InputField type="password" label='Label' size='md' isWrong={true}/>
       <InputField type="password" label='Label' size='lg' isWrong={false}/> */}
-{/* 
-      <div>
-    <ListItem />
+
+    {
+    /*List-QnA*/
+    /* <div>
+      <List items={items} size="large" />
     </div>
     <div>
-    <ListItem  size="large" />
+      <List items={items} size="small" />
     </div> */}
+
 
  {/* <Checkbox size="small" text="I consent to having my data processed according to the privacy statement" />
     <Checkbox size="medium" text="I consent to having my data processed according to the privacy statement" />
@@ -62,6 +67,8 @@ function App() {
 
     { /*<FooterWithLogo />*/ }
 
+    {/* <ListItem size="large" text="Are you committed to quality maintenance and friendly customer service?" />
+    <ListItem size="small" text="Are you committed to quality maintenance and friendly customer service?" /> */}
 
     </>
   )
