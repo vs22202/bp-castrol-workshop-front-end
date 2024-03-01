49ce9ce7ca8689d72c50c94515d0d97543abeaeb
Vishaal <vishaal.s2020@vitstudent.ac.in>
2024-03-02 03:41:27 +0530

    Integrated Application Upload With Backend API

--- src/components/ApplicationUploadPage/ApplicationUpload.tsx
+++ src/components/ApplicationUploadPage/ApplicationUpload.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Checkbox } from "../CheckboxComponent/CheckboxComponent";
 import { InputField } from "../InputFieldComponent/InputField";
 import { Button } from "../ButtonComponent/Button";
@@ -11,10 +11,14 @@ import {
   FieldValues,
   FormProvider,
 } from "react-hook-form";
-import { Input } from "components/FormInputs";
+import { ApplicationInputFields } from "../FormInputs";
 import { Option } from "components/DropDownComponent/Option";
 import inputs from "./ApplicationUploadFormFields";
 import { renderInput } from "../FormFieldRenderLogic";
+import AuthContext, { AuthContextProps } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import AlertContext, { AlertContextProps } from "../../contexts/AlertContext";
+import styles from "./ApplicationUpload.module.css";
 // import { renderInput } from "../FormFieldRenderLogic";
 interface FormUtilsProps {
   register: any;
@@ -22,7 +26,10 @@ interface FormUtilsProps {
 }
 
 const ApplicationUpload: React.FC = () => {
-  const methods = useForm<Input>();
+  const methods = useForm<ApplicationInputFields>();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { sendAlert } = useContext(AlertContext) as AlertContextProps;
   const {
     register,
     handleSubmit,
@@ -30,152 +37,93 @@ const ApplicationUpload: React.FC = () => {
     formState: { errors },
     trigger,
     control,
-  } = methods
+  } = methods;
 
-
+  const { currentUser } = useContext(AuthContext) as AuthContextProps;
   const handleInputChange = async (event: any) => {
     const name = event?.target.name;
-  
+
     // Find the corresponding input field in the inputs array
     const inputField = inputs.find((input) => input.name === name);
-  
+
     // Check if the input field is of type "input" or "checkbox"
-    if (inputField && (inputField.type === 'text' || inputField.type === 'checkbox')) {
+    if (
+      inputField &&
+      (inputField.type === "text" || inputField.type === "checkbox")
+    ) {
       // Trigger validation for the specified input fields
       await trigger(name);
     }
-  
+
     console.log(errors);
   };
-  
-/*   const renderInput = (input: Input, { register, errors }: FormUtilsProps) => {
-    switch (input.type) {
-      case "checkbox":
-        return (
-          <Checkbox
-            key={input.id}
-            name={input.name}
-            size="medium"
-            text={input.label}
-            register={register}
-            errors={errors}
-            validationSchema={{
-              required: input.required,
-            }}
-            required={input.required}
-            //OnChange={(e)=>field.onChange(e.target.checked)}
-            //checked={field.value}
-          />
-        );
-      case "text":
-        return (
-          <>
-            <InputField
-              key={input.id}
-              type="text"
-              name={input.name}
-              label={input.label}
-              size="large"
-              register={register}
-              required={input.required}
-              maxlen={input.maxlen}
-              errors={errors}
-              placeholder={input.placeholder}
-              
-              validationSchema={{
-                required: input.required,
-                minLength: {
-                  value: input.minlen,
-                  message: input.errorMessage,
-                },
-                maxLength: {
-                  value: input.maxlen,
-                  message: input.errorMessage,
-                },
-                pattern: {
-                  value: input.pattern,
-                  message: `${input.label} format is wrong.`,
-                },
-              }}
-            />
-          </>
-        );
 
-      case "dropdown":
-        const defaultValue = input.compulsorylist
-          ? input.compulsorylist.map((option) => option.value).join(",")
-          : undefined;
-        return (
-          <>
-            <Controller
-              name={input.name}
-              control={control as unknown as Control<FieldValues>}
-              defaultValue={defaultValue}
-              render={({ field: { onChange, value } }) => (
-                <DropDown
-                  name={input.name}
-                  key={input.id}
-                  //value
-                  placeholder={input.label}
-                  // register={register}
-                  compulsoryList={input.compulsorylist}
-                  optionList={input.optionlist}
-                  // {...field}
-                  required={input.required}
-                  register={register}
-                  errors={errors}
-                  onchange={onChange}
-                  validationSchema={{
-                    required: input.required,
-                  }}
-                />
-              )}
-            />
-          </>
-        );
-
-      default:
-
-      //file/image upload inputs
+  const submitForm: SubmitHandler<ApplicationInputFields> = async (data) => {
+    setLoading(true);
+    const formData = new FormData();
+    let key: keyof ApplicationInputFields;
+    console.log(data.files);
+    for (key in data) {
+      if (key == "files") {
+        continue;
+      }
+      formData.append(key, `${data[key]}`);
     }
-  };
- */
-  
-  const submitForm: SubmitHandler<Input> = (data) => {
-    console.log(data); //submit funciton
+    for (const file in data.files) formData.append("files", data.files[file]);
+    formData.set("user_email", currentUser?.user_email || "");
+    console.log(formData.getAll("files"));
+    try {
+      const result = await fetch("http://localhost:3000/application", {
+        method: "POST",
+        body: formData,
+      });
+      const res = await result.json();
+      if (res.output == "success") {
+        console.log("hello");
+        setLoading(false);
+        sendAlert({
+          message: "Application Submitted Succesfully",
+          type: "success",
+        });
+        navigate("/", { replace: true });
+        return;
+      }
+      sendAlert({ message: res.msg, type: "error" });
+    } catch (err) {
+      console.log(err);
+    }
+    setLoading(false);
   };
 
   return (
-    <div className="appContainer">
-      <div className="contentContainer">
-        <div className="formContainer">
-          <FormProvider {...methods}>
-            <form onSubmit={handleSubmit(submitForm)} onChange={handleInputChange}>
-              <h1
-                style={{
-                  color: "rgba(0, 153, 0, 1)",
-                  fontSize: "28px",
-                  textAlign: "left",
-                  fontWeight: "bold",
-                }}
-              >
-                Certified Castrol Workshop Application
-              </h1>
-              <h2
-                style={{
-                  color: "rgba(102, 102, 102, 1)",
-                  fontSize: "20px",
-                  textAlign: "left",
-                }}
-              >
-                Take your workshop to the next level!
-              </h2>
-              {inputs.map((input) => renderInput(input, { register, errors, control }))}
-              <Button text="Submit" size="sm" type="solid" action="submit"/>
-            </form>
-          </FormProvider>
-        </div>
-      </div>
+    <div className={`${styles.formContainer} ${loading ? styles.loadingState : ""}`}>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(submitForm)} onChange={handleInputChange}>
+          <h1
+            style={{
+              color: "rgba(0, 153, 0, 1)",
+              fontSize: "28px",
+              textAlign: "left",
+              fontWeight: "bold",
+            }}
+          >
+            Certified Castrol Workshop Application
+          </h1>
+          <h2
+            style={{
+              color: "rgba(102, 102, 102, 1)",
+              fontSize: "20px",
+              textAlign: "left",
+            }}
+          >
+            Take your workshop to the next level!
+          </h2>
+          {inputs.map((input) =>
+            renderInput(input, { register, errors, control })
+          )}
+          <Button text="Submit" size="sm" type="solid" action="submit" />
+        </form>
+      </FormProvider>
     </div>
   );
 };
--- src/components/FormFieldRenderLogic.tsx
+++ src/components/FormFieldRenderLogic.tsx
@@ -1,130 +1,147 @@
 import React, { useEffect, useState } from "react";
-import { InputField } from '../components/InputFieldComponent/InputField';
-import { Checkbox } from '../components/CheckboxComponent/CheckboxComponent';
+import { InputField } from "../components/InputFieldComponent/InputField";
+import { Checkbox } from "../components/CheckboxComponent/CheckboxComponent";
 import { Input } from "./FormInputs";
-import { Control, Controller, FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import {
+  Control,
+  Controller,
+  FieldValues,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
 import { DropDown } from "./DropDownComponent/DropDown";
 import ListItem from "./Q&AComponent/ListItem";
 import FileGrid from "./FileGridComponent/FileGrid";
 
 interface FormUtilsProps {
-    register: any;
-    errors: any;
-    control?:any;
+  register: any;
+  errors: any;
+  control?: any;
 }
 
-export const renderInput = (input: Input, { register, errors, control }: FormUtilsProps) => {
+export const renderInput = (
+  input: Input,
+  { register, errors, control }: FormUtilsProps
+) => {
+  const [screenSize, setScreenSize] = useState(getScreenSize());
 
-    const [screenSize, setScreenSize] = useState(getScreenSize())
-
-    function getScreenSize(){
-        if(window.innerWidth < 500){
-          return "small"
-        }else if(window.innerWidth < 1000){
-          return "medium"
-        }else{
-          return "large"
-        }
-      }
-    
-      function handleResize(){
-        setScreenSize(getScreenSize)
-      }
-    
-      useEffect(() => {
-        window.addEventListener("resize", handleResize);
-        return () => {
-          window.removeEventListener("resize", handleResize);
-        };
-      }, []);
-
-    switch (input.type) {
-        case "checkbox":
-            return (
-                <Checkbox
-                    key={input.id}
-                    name={input.name}
-                    //size={input.size ? input.size : "medium"}
-                    size={screenSize}
-                    text={input.label}
-                    register={register}
-                    errors={errors}
-                    validationSchema={{
-                        required:input.required,
-                    }}
-                    required={input.required}
-                />
-            );
-        case "dropdown":
-            const defaultValue = input.compulsorylist ? input.compulsorylist.map((option) => option.value).join(',') : undefined;
-            return(
-                <>
-                <Controller
-                name={input.name}
-                control={control as unknown as Control<FieldValues>}
-                defaultValue={defaultValue}
-                render={({field:{onChange, value}})=>(
-                    <DropDown
-                        name={input.name}
-                        key={input.id}
-                        //value
-                        placeholder={input.label}
-                        // register={register}
-                        compulsoryList={input.compulsorylist}
-                        optionList= {input.optionlist}
-                        // {...field}
-                        size={screenSize}
-                        required={input.required}
-                        register={register}
-                        errors={errors}
-                        onchange={onChange}
-                        validationSchema={{
-                        required: input.required,
-                    }}
-                    />
-                )}
-                />
-                </>
-            ) 
-        case "text":
-            return (
-                <InputField
-                    key={input.id}
-                    type={input.text_type ? input.text_type : "text"}
-                    name={input.name}
-                    label={input.label}
-                    size={screenSize}
-                    //size={input.size ? input.size : "medium"}
-                    register={register}
-                    maxlen={input.maxlen}
-                    errors={errors}
-                    required={input.required}
-                    placeholder={input.placeholder}
-                    validationSchema={{
-                        required: input.required,
-                        minLength: {
-                            value: input.minlen,
-                            message: input.errorMessage
-                        },
-                        maxLength: {
-                            value: input.maxlen,
-                            message: input.errorMessage
-                        },
-                        pattern: {
-                            value: input.pattern,
-                            message: input.errorMessage
-                        },
-                    }}
-                />
-            );
-        default:
-            // Handle other input types or return null
-            return (
-                <>
-                <FileGrid/>
-                <ListItem size={screenSize} text="Upload the videos and images of workshop and garage"/>
-                <ListItem size={screenSize}text="The images and videos of the services provided by the workshop can also be uploaded"/>
-                </>
-            );
+  function getScreenSize() {
+    if (window.innerWidth < 500) {
+      return "small";
+    } else if (window.innerWidth < 1000) {
+      return "medium";
+    } else {
+      return "large";
     }
-}
+  }
+
+  function handleResize() {
+    setScreenSize(getScreenSize);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  switch (input.type) {
+    case "checkbox":
+      return (
+        <Checkbox
+          key={input.id}
+          name={input.name}
+          //size={input.size ? input.size : "medium"}
+          size={screenSize}
+          text={input.label}
+          register={register}
+          errors={errors}
+          validationSchema={{
+            required: input.required,
+          }}
+          required={input.required}
+        />
+      );
+    case "dropdown":
+      const defaultValue = input.compulsorylist
+        ? input.compulsorylist.map((option) => option.value).join(",")
+        : undefined;
+      return (
+        <>
+          <Controller
+            name={input.name}
+            control={control as unknown as Control<FieldValues>}
+            defaultValue={defaultValue}
+            render={({ field: { onChange, value } }) => (
+              <DropDown
+                name={input.name}
+                key={input.id}
+                //value
+                placeholder={input.label}
+                // register={register}
+                compulsoryList={input.compulsorylist}
+                optionList={input.optionlist}
+                // {...field}
+                size={screenSize}
+                required={input.required}
+                register={register}
+                errors={errors}
+                onchange={onChange}
+                validationSchema={{
+                  required: input.required,
+                }}
+              />
+            )}
+          />
+        </>
+      );
+    case "text":
+    case "hidden":
+      return (
+        <InputField
+          key={input.id}
+          type={input.text_type ? input.text_type : "text"}
+          name={input.name}
+          label={input.label}
+          size={screenSize}
+          //size={input.size ? input.size : "medium"}
+          register={register}
+          maxlen={input.maxlen}
+          errors={errors}
+          required={input.required}
+          placeholder={input.placeholder}
+          validationSchema={{
+            required: input.required,
+            minLength: {
+              value: input.minlen,
+              message: input.errorMessage,
+            },
+            maxLength: {
+              value: input.maxlen,
+              message: input.errorMessage,
+            },
+            pattern: {
+              value: input.pattern,
+              message: input.errorMessage,
+            },
+          }}
+        />
+      );
+    default:
+      // Handle other input types or return null
+      return (
+        <>
+          <FileGrid name="files" />
+          <ListItem
+            size={screenSize}
+            text="Upload the videos and images of workshop and garage"
+          />
+          <ListItem
+            size={screenSize}
+            text="The images and videos of the services provided by the workshop can also be uploaded"
+          />
+        </>
+      );
+  }
+};
