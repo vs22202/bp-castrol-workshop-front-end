c20567344f47ef4331c6833cb7abcb2585080e07
Vishaal <vishaal.s2020@vitstudent.ac.in>
2024-03-01 10:35:08 +0530

    Small Redesign of Alert Component

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,11 @@ import { Icon } from "./components/IconComponent/Icon";
 import { LoginPage } from "./components/LoginPage/LoginPage";
 import { SignupPage } from "./components/SignupPage/SignupPage";
 import { Alert } from "./components/AlertComponent/Alert";
-import AlertContext ,{ AlertContextProps} from "./contexts/AlertContext";
+import AlertContext, { AlertContextProps } from "./contexts/AlertContext";
 import { useContext } from "react";
 
 function App() {
-  const { alert } = useContext(AlertContext) as AlertContextProps
+  const { alert } = useContext(AlertContext) as AlertContextProps;
   /*for list*/
   const items = [
     "Are you committed to quality maintenance and friendly customer service?",
