cfa9da743f850fd78a43dfca7105d53d720a7b89
Vishaal <vishaal.s2020@vitstudent.ac.in>
2024-03-13 15:00:17 +0530

    Fixed Styles Issues IN Jest Test Files

--- jest.config.ts
+++ jest.config.ts
@@ -3,11 +3,12 @@ export default {
     roots:['<rootDir>/src'],
     testEnvironment: 'jest-environment-jsdom',
     transform: {
-        "^.+\\.tsx?$": "ts-jest" 
+        "^.+\\.tsx?$": "ts-jest",
+        ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform"
     // process `*.tsx` files with `ts-jest`
     },
     moduleNameMapper: {
         '\\.(gif|ttf|eot|png)$': '<rootDir>/src/test/__ mocks __/fileMock.js',
-        '^.+\\.(css|less|svg)$': "identity-obj-proxy"
+        '^.+\\.(svg)$': "identity-obj-proxy"
     },
 }
